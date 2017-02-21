0b978b1fd8ff21e311c0275893d6098e19277b27
Unknown <thenitronos@gmail.com>
2017-02-21 15:19:11 +0100

    Doc Distance methods

--- src/ch/epfl/alpano/Distance.java
+++ src/ch/epfl/alpano/Distance.java
@@ -1,15 +1,27 @@
 package ch.epfl.alpano;
 
 public interface Distance {
-    
+    /**
+     * earth's radius in meters
+     */
     public static double EARTH_RADIUS = 6371000;
     
+    /**
+     * transform a distance on earth into an angle
+     * @param distanceInMeters to transform
+     * @return radian angle
+     */
     public static double toRadians(double distanceInMeters) {
        double angle = distanceInMeters/EARTH_RADIUS;
        
        return angle;
     }
     
+    /**
+     * transform an angle into a distance on earth
+     * @param distanceInRadians to transform
+     * @return distance in meters
+     */
     public static double toMeters(double distanceInRadians) {
         double distance = distanceInRadians*EARTH_RADIUS;
         
