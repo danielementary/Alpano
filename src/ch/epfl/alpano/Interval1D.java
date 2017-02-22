f6fef9d31fbfe3648e1b483b7ee65c0ae04b3200
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-02-23 00:36:04 +0100

    Interval1D elementary methods added

--- src/ch/epfl/alpano/Interval1D.java
+++ src/ch/epfl/alpano/Interval1D.java
@@ -7,6 +7,61 @@ package ch.epfl.alpano;
  *
  */
 
-public class Interval1D {
-
+public final class Interval1D {
+    
+    /**
+     * lower bound included
+     */
+    private final int includedFrom;
+    /**
+     * upperBound included
+     */
+    private final int includedTo;
+    
+    /**
+     * Creates an instance of an interval
+     * @param includedFrom lower bound
+     * @param includedTo upper bound
+     */
+    public Interval1D(int includedFrom, int includedTo) {
+        Preconditions.checkArgument(includedTo >= includedFrom);
+        
+        this.includedFrom = includedFrom;
+        this.includedTo = includedTo;
+    }
+    
+    /**
+     * gives the lower bound of the instance (includedFrom)
+     * @return the interval's lower bound in int
+     */
+    public final int includedFrom() {
+        return includedFrom;
+    }
+    
+    /**
+     * gives the upper bound of the instance (includedTo)
+     * @return the interval's upper bound in int
+     */
+    public final int includedTo() {
+        return includedTo;
+    }
+    
+    /**
+     * gives the number of elements in the interval
+     * @return size of interval in int
+     */
+    public final int size() {
+        return includedTo-includedFrom+1;
+    }
+    
+    public final int sizeOfIntersectionWith(Interval1D that) {
+        int thisFrom = this.includedFrom();
+        int thisTo = this.includedTo();
+        int thatFrom = that.includedFrom();
+        int thatTo = that.includedTo();
+        
+        //à compléter
+        
+        return 0;
+    }
 }
