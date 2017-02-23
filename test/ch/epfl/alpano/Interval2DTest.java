323473354133b81d316defb31d64375b38708503
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-02-23 13:00:42 +0100

    Interval2D created

---
+++ test/ch/epfl/alpano/Interval2DTest.java
@@ -0,0 +1,14 @@
+package ch.epfl.alpano;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Interval2DTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
