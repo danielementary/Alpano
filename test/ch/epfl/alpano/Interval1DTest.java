83f04a7fd44ea8fe1da2158dabfee3f0c40daa33
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-02-25 15:11:22 +0100

    interval1d test cases added

--- test/ch/epfl/alpano/Interval1DTest.java
+++ test/ch/epfl/alpano/Interval1DTest.java
@@ -6,12 +6,13 @@
 
 package ch.epfl.alpano;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
 public class Interval1DTest {
-    
     private Interval1D inter1 = new Interval1D(1,5);
     private Interval1D inter2 = new Interval1D(2,9);
     private Interval1D inter3 = new Interval1D(-3,4);
@@ -21,46 +22,44 @@ public class Interval1DTest {
     private Interval1D inter7 = new Interval1D(-6,-2);
     
     @Test(expected = IllegalArgumentException.class)
-    public void creationException(){
+    public void creationException() {
         new Interval1D(-2,-5);
     }
     
     @Test
-    public void creation(){
+    public void creation() {
         new Interval1D(1,4);
     }
     
     @Test 
-    public void getterSetterWorks(){
-        
+    public void gettersWork() {
         assertEquals(1, inter1.includedFrom(), 0);
         assertEquals(5, inter1.includedTo(), 0);
-        
     }
     
 
     @Test
-    public void intersectionSizeWorks() {       
-        
+    public void intersectionSizeWorks() {
         assertEquals(4, inter1.sizeOfIntersectionWith(inter2));
         assertEquals(4, inter1.sizeOfIntersectionWith(inter3));
         assertEquals(0, inter1.sizeOfIntersectionWith(inter4));
-        assertEquals(inter1.size(), inter1.sizeOfIntersectionWith(inter1), 0);
-        assertEquals(inter5.size(), inter3.sizeOfIntersectionWith(inter5), 0);
-        
+        assertEquals(5, inter1.sizeOfIntersectionWith(inter1), 0);
+        assertEquals(5, inter3.sizeOfIntersectionWith(inter5), 0);
     }
     
     @Test
-    public void boundingUnionWorks(){
-
+    public void boundingUnionWorks() {
         assertTrue(inter1.boundingUnion(inter2).equals(new Interval1D(1,9)));
         assertFalse(inter1.boundingUnion(inter2).equals(new Interval1D(-1,9)));
         
+        assertTrue(inter2.boundingUnion(inter1).equals(new Interval1D(1,9)));
+        assertFalse(inter2.boundingUnion(inter1).equals(new Interval1D(-1,9)));
+
+        assertTrue(inter4.boundingUnion(inter4).equals(new Interval1D(0,0)));
     }
     
     @Test
-    public void equalsTest(){
-        
+    public void equalsTest() {
         assertTrue(inter1.equals(inter1));
         assertFalse(inter1.equals(inter2));
         assertTrue(inter1.equals(inter6));
@@ -68,35 +67,43 @@ public class Interval1DTest {
     }
     
     @Test
-    public void toStringTest(){
+    public void toStringTest() {
         assertEquals("[1..5]", inter1.toString());
         assertEquals("[0..0]", inter4.toString());
         assertEquals("[-2..2]", inter5.toString());
     }
     
     @Test
-    public void containsTest(){
+    public void containsTest() {
         assertTrue(inter1.contains(2));
         assertTrue(inter4.contains(0));
         assertFalse(inter3.contains(1000));
-        
     }
     
     @Test
-    public void isUnionableWithTest(){
+    public void isUnionableWithTest() {
         assertTrue(inter1.isUnionableWith(inter2));
         assertTrue(inter2.isUnionableWith(inter3));
+        
+        assertTrue(inter3.isUnionableWith(inter5));
+        assertTrue(inter5.isUnionableWith(inter3));
+        
+        assertTrue(inter3.isUnionableWith(inter3));
+        assertTrue(inter4.isUnionableWith(inter4));
+        
         assertFalse(inter1.isUnionableWith(inter7));
     }
     
     @Test
-    public void unionTest(){
+    public void unionTest() {
         assertTrue((inter1.union(inter2)).equals(new Interval1D(1,9)));
         assertTrue((inter2.union(inter3)).equals(new Interval1D(-3,9)));
+
+        assertTrue((inter2.union(inter2)).equals(inter2));
     }
     
     @Test (expected = IllegalArgumentException.class)
-    public void unionErrorTest(){
+    public void unionErrorTest() {
         inter1.union(inter7);
     }
 }
