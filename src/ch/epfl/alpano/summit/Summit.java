34b824a78bbb115437337da2fa7932c27af6e3c1
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-03-13 13:36:18 +0100

    package ...summit and beginning Summit class

---
+++ src/ch/epfl/alpano/summit/Summit.java
@@ -0,0 +1,44 @@
+package ch.epfl.alpano.summit;
+
+/**
+ * 
+ * @author Samuel Chassot (270955)
+ * @author Daniel Filipe Nunes Silva (275197)
+ *
+ */
+
+import ch.epfl.alpano.GeoPoint;
+
+public final class Summit {
+    
+    private String name;
+    private GeoPoint position;
+    private int elevation;
+    
+    public Summit(String name, GeoPoint position, int elevation) {
+        this.name = name;
+        this.position = position;
+        this.elevation = elevation;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the position
+     */
+    public GeoPoint getPosition() {
+        return position;
+    }
+
+    /**
+     * @return the elevation
+     */
+    public int getElevation() {
+        return elevation;
+    }
+}
