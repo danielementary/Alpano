76be1e819f8d03f56196df2ede39707b698774da
Unknown <samuel.chassot@bluewin.ch>
2017-03-15 18:54:27 +0100

    again

---
+++ test/ch/epfl/alpano/PanoramaParametersTest.java
@@ -0,0 +1,35 @@
+/*
+ *	Author:      Samuel Chassot (270955)
+ *	Date:        15 mars 2017
+ */
+
+
+package ch.epfl.alpano;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PanoramaParametersTest {
+
+    @Test
+    public void testAzimuthForX() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testXForAzimuth() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAltitudeForY() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testYForAltitude() {
+        fail("Not yet implemented");
+    }
+
+}
