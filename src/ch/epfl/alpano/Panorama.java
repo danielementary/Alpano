785d3ee6fe04492351e5af57d0f671295cf28656
Unknown <samuel.chassot@bluewin.ch>
2017-03-20 15:16:23 +0100

    panoramaParameters

--- src/ch/epfl/alpano/Panorama.java
+++ src/ch/epfl/alpano/Panorama.java
@@ -2,6 +2,8 @@
 
 package ch.epfl.alpano;
 
+import java.util.Arrays;
+
 /**
  * 
  * @author Samuel Chassot (270955)
@@ -11,23 +13,24 @@ package ch.epfl.alpano;
 
 public final class Panorama {
 
-    PanoramaParameters parameters;
-    float[] distance;
-    float[] longitude;
-    float[] latitude;
-    float[] altitude;
-    float[] slope;
+    private PanoramaParameters parameters;
+
+    private float[] distance;
+    private float[] longitude;
+    private float[] latitude;
+    private float[] elevation;
+    private float[] slope;
 
 
     public Panorama(PanoramaParameters parameters, float[] distance,
-            float[] longitude, float[] latitude, float[] altitude,
+            float[] longitude, float[] latitude, float[] elevation,
             float[] slope) {
         
         this.parameters = parameters;
         this.distance = distance;
         this.longitude = longitude;
         this.latitude = latitude;
-        this.altitude = altitude;
+        this.elevation = elevation;
         this.slope = slope;
     }
 
@@ -61,12 +64,12 @@ public final class Panorama {
         return latitude[index];
     }
 
-    public float altitudeAt(int x, int y){
+    public float elevationAt(int x, int y){
     	if(!parameters.isValidSampleIndex(x,y)){
     		throw new IndexOutOfBoundsException();
     	}
         int index = parameters.linearSampleIndex(x, y);
-        return altitude[index];
+        return elevation[index];
     }
 
     public float slopeAt(int x, int y){
@@ -75,5 +78,97 @@ public final class Panorama {
     	}
         int index = parameters.linearSampleIndex(x, y);
         return slope[index];
+    }
+    
+    public float distanceAt(int x, int y, float d){
+        if(!parameters.isValidSampleIndex(x,y)){
+            return d;
+        }
+        int index = parameters.linearSampleIndex(x, y);
+        return distance[index];
+    }
+    
+    
+    
+    public static final class Builder{
+        
+        private PanoramaParameters parameters;
+        private float[] distance;
+        private float[] longitude;
+        private float[] latitude;
+        private float[] elevation;
+        private float[] slope;
+
+        private boolean FLAG = false;
+        
+
+        public Builder(PanoramaParameters parameters) {
+            
+            Preconditions.checkArgument(parameters != null);
+            this.parameters = parameters;
+            
+            distance = new float[parameters.width()*parameters.height()];
+            longitude = new float[parameters.width()*parameters.height()];
+            latitude = new float[parameters.width()*parameters.height()];
+            elevation = new float[parameters.width()*parameters.height()];
+            slope = new float[parameters.width()*parameters.height()];
+
+
+            
+            Arrays.fill(distance, Float.POSITIVE_INFINITY);
+                                
+        }
+        
+        public Builder setDistanceAt(int x, int y, float distance){
+        	if(FLAG){
+        		throw new IllegalStateException();
+        	}
+        	this.distance[parameters.linearSampleIndex(x,y)] = distance;
+
+        	return this;
+        }
+        public Builder setLongitudeAt(int x, int y, float longitude){
+        	if(FLAG){
+        		throw new IllegalStateException();
+        	}
+        	this.longitude[parameters.linearSampleIndex(x,y)] = longitude;
+
+        	return this;
+        }
+        public Builder setLatitudeAt(int x, int y, float latitude){
+        	if(FLAG){
+        		throw new IllegalStateException();
+        	}
+        	this.latitude[parameters.linearSampleIndex(x,y)] = latitude;
+
+        	return this;
+        }
+        public Builder setElevationAt(int x, int y, float elevation){
+        	if(FLAG){
+        		throw new IllegalStateException();
+        	}
+        	this.elevation[parameters.linearSampleIndex(x,y)] = elevation;
+
+        	return this;
+        }
+        public Builder setSlopeAt(int x, int y, float slope){
+        	if(FLAG){
+        		throw new IllegalStateException();
+        	}
+        	this.slope[parameters.linearSampleIndex(x,y)] = slope;
+
+        	return this;
+        }
+        
+        public Panorama build(){
+            if (FLAG){
+                throw new IllegalStateException();
+            }
+            FLAG = true;
+            return new Panorama(parameters, distance, longitude, latitude, elevation, slope);
+        }
+        
+        
+        
     }
 }
