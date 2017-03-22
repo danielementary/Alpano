735aa2df1203919fcdc911d2091f019a4ee2f457
Unknown <samuel.chassot@bluewin.ch>
2017-03-22 19:33:17 +0100

    all this shit is test

--- src/ch/epfl/alpano/DrawPanorama2.java
+++ src/ch/epfl/alpano/DrawPanorama2.java
@@ -3,6 +3,7 @@ package ch.epfl.alpano;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 import static java.lang.Math.*;
@@ -27,14 +28,14 @@ final class DrawPanorama2 {
     final static File HGT_FILE3 = new File("N45E007.hgt");
     final static File HGT_FILE4 = new File("N45E006.hgt");
 
-    final static int IMAGE_WIDTH = 3000;
-    final static int IMAGE_HEIGHT = 1200;
+    final static int IMAGE_WIDTH = 4000;
+    final static int IMAGE_HEIGHT = 1500;
 
     final static double ORIGIN_LON = Math.toRadians(6.88440);
-    final static double ORIGIN_LAT = Math.toRadians(46.56642);
-    final static int ELEVATION = 950;
+    final static double ORIGIN_LAT = Math.toRadians(46.56642 );
+    final static int ELEVATION = 900;
     final static double CENTER_AZIMUTH = Math.toRadians(178.18);
-    final static double HORIZONTAL_FOV = Math.toRadians(80);
+    final static double HORIZONTAL_FOV = Math.toRadians(100);
     final static int MAX_DISTANCE = 150_000;
 
     final static PanoramaParameters PARAMS =
@@ -48,20 +49,19 @@ final class DrawPanorama2 {
                              IMAGE_HEIGHT);
 
     public static void main(String[] as) throws Exception {
-        try (DiscreteElevationModel dDEM =
-                new HgtDiscreteElevationModel(HGT_FILE)) {
-//             ContinuousElevationModel cDEM =
-//               new ContinuousElevationModel(dDEM);
-//             Panorama p = new PanoramaComputer(cDEM)
-//               .computePanorama(PARAMS);
+        
              try{
+                 DiscreteElevationModel dDEM1 =
+                         new HgtDiscreteElevationModel(HGT_FILE);
                  DiscreteElevationModel dDEM2 =
                          new HgtDiscreteElevationModel(HGT_FILE2);
                  DiscreteElevationModel dDEM3 =
                          new HgtDiscreteElevationModel(HGT_FILE3);
                  DiscreteElevationModel dDEM4 =
                          new HgtDiscreteElevationModel(HGT_FILE4);
-                 DiscreteElevationModel dDEM12 = dDEM.union(dDEM2);
+                 
+                 
+                 DiscreteElevationModel dDEM12 = dDEM1.union(dDEM2);
                  DiscreteElevationModel dDEM34 = dDEM3.union(dDEM4);
                  
                  DiscreteElevationModel dDEM_final = dDEM12.union(dDEM34);
@@ -80,16 +80,20 @@ final class DrawPanorama2 {
              for (int x = 0; x < IMAGE_WIDTH; ++x) {
                for (int y = 0; y < IMAGE_HEIGHT; ++y) {
                  float d = p.distanceAt(x, y);
+                 float s = p.slopeAt(x, y);
                  int c = (d == Float.POSITIVE_INFINITY)
                    ? 0x87_CE_EB
-                   : gray((d - 2_000) / 15_000);
+//                   : gray((d - 2_000) / 40_000);
+                   :color(d,s);
+                 
                  i.setRGB(x, y, c);
                }
              }
              
 
-        ImageIO.write(i, "png", new File("test.png"));
+        ImageIO.write(i, "png", new File("Zenias_view.png"));
         
+        dDEM1.close();
         dDEM2.close();
         dDEM3.close();
         dDEM4.close();
@@ -99,11 +103,33 @@ final class DrawPanorama2 {
         
       }
         }
-    }
+    
     
     private static int gray(double v) {
         double clampedV = max(0, min(v, 1));
         int gray = (int) (255.9999 * clampedV);
         return (gray << 16) | (gray << 8) | gray;
       }
+    
+    private static int color (double d, double s){
+        Color color;
+        
+        if(d < 2_000){
+            color = new Color(200,255,255);
+        }else if(d < 10_000){
+            color = new Color(100,255,100);
+        }else if (d < 30_000){
+            color = new Color(255,150,100);
+        }else if (d < 50_000){
+            color = new Color(0,255,255);
+        }else{
+            color = new Color(0,175,180);
+        }
+        
+        color = new Color(Math.max(0, color.getRed() - (int) (s*230)),
+                Math.max(0, color.getGreen() - (int) (s*230)),
+                Math.max(0, color.getBlue() - (int) (s*230)));
+        
+        return color.getRGB();
+    }
   }
