2a44a16d28aeb7ea2c95d298d0d07ee3fb486888
Unknown <samuel.chassot@bluewin.ch>
2017-03-22 11:20:00 +0100

    test dessin

--- src/ch/epfl/alpano/dem/DrawPanorama2.java
+++ src/ch/epfl/alpano/dem/DrawPanorama2.java
@@ -29,16 +29,15 @@ import ch.epfl.alpano.dem.HgtDiscreteElevationModel;
 
 final class DrawPanorama2 {
     final static File HGT_FILE = new File("N46E007.hgt");
-    final static File HGT_FILE2 = new File("N45E007.hgt");
 
-    final static int IMAGE_WIDTH = 1500;
-    final static int IMAGE_HEIGHT = 600;
+    final static int IMAGE_WIDTH = 3000;
+    final static int IMAGE_HEIGHT = 1200;
 
-    final static double ORIGIN_LON = toRadians(7.62191);
-    final static double ORIGIN_LAT = toRadians(46.05032);
+    final static double ORIGIN_LON = Math.toRadians(7.05097);
+    final static double ORIGIN_LAT = Math.toRadians(46.62305);
     final static int ELEVATION = 800;
-    final static double CENTER_AZIMUTH = toRadians(270);
-    final static double HORIZONTAL_FOV = toRadians(65);
+    final static double CENTER_AZIMUTH = Math.toRadians(174.95);
+    final static double HORIZONTAL_FOV = Math.toRadians(80);
     final static int MAX_DISTANCE = 100_000;
 
     final static PanoramaParameters PARAMS =
@@ -52,27 +51,27 @@ final class DrawPanorama2 {
                              IMAGE_HEIGHT);
 
     public static void main(String[] as) throws Exception {
-      try (DiscreteElevationModel dDEM = new CompositeDiscreteElevationModel(
-           new HgtDiscreteElevationModel(HGT_FILE), new HgtDiscreteElevationModel(HGT_FILE2))) {
-        ContinuousElevationModel cDEM =
-          new ContinuousElevationModel(dDEM);
-        Panorama p = new PanoramaComputer(cDEM)
-          .computePanorama(PARAMS);
+        try (DiscreteElevationModel dDEM =
+                new HgtDiscreteElevationModel(HGT_FILE)) {
+             ContinuousElevationModel cDEM =
+               new ContinuousElevationModel(dDEM);
+             Panorama p = new PanoramaComputer(cDEM)
+               .computePanorama(PARAMS);
 
-        BufferedImage i =
-          new BufferedImage(IMAGE_WIDTH,
-                            IMAGE_HEIGHT,
-                            TYPE_INT_RGB);
+             BufferedImage i =
+               new BufferedImage(IMAGE_WIDTH,
+                                 IMAGE_HEIGHT,
+                                 TYPE_INT_RGB);
 
-        for (int x = 0; x < IMAGE_WIDTH; ++x) {
-          for (int y = 0; y < IMAGE_HEIGHT; ++y) {
-            float d = p.distanceAt(x, y);
-            int c = (d == Float.POSITIVE_INFINITY)
-              ? 0x87_CE_EB
-              : gray((d - 2_000) / 15_000);
-            i.setRGB(x, y, c);
-          }
-        }
+             for (int x = 0; x < IMAGE_WIDTH; ++x) {
+               for (int y = 0; y < IMAGE_HEIGHT; ++y) {
+                 float d = p.distanceAt(x, y);
+                 int c = (d == Float.POSITIVE_INFINITY)
+                   ? 0x87_CE_EB
+                   : gray((d - 2_000) / 15_000);
+                 i.setRGB(x, y, c);
+               }
+             }
 
         ImageIO.write(i, "png", new File("test.png"));
       }
