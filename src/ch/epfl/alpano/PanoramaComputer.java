ce8be89c00181d16cb3255adc2e8887c2e55cd6c
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-03-25 22:36:02 +0100

    panorama clean up

--- src/ch/epfl/alpano/PanoramaComputer.java
+++ src/ch/epfl/alpano/PanoramaComputer.java
@@ -1,50 +1,58 @@
 package ch.epfl.alpano;
 
+/**
+ * 
+ * @author Samuel Chassot (270955)
+ * @author Daniel Filipe Nunes Silva (275197)
+ *
+ */
+
 import java.util.function.DoubleUnaryOperator;
+import static java.util.Objects.requireNonNull;
 
 import ch.epfl.alpano.dem.ContinuousElevationModel;
 import ch.epfl.alpano.dem.ElevationProfile;
-
-import static java.util.Objects.requireNonNull;
-
 
 public final class PanoramaComputer {
     
     private final ContinuousElevationModel dem;
     
-    public PanoramaComputer(ContinuousElevationModel dem){
+    public PanoramaComputer(ContinuousElevationModel dem) {
         this.dem = requireNonNull(dem);
     }
     
-    public Panorama computePanorama(PanoramaParameters parameters){
+    public Panorama computePanorama(PanoramaParameters parameters) {
         
         Panorama.Builder builder = new Panorama.Builder(parameters);
+        
         double step = Math.scalb(1, 6);
         double delta = Math.scalb(1, 2);
         
-        for (int i = 0 ; i < parameters.width() ; ++i){
+        for (int i = 0 ; i < parameters.width() ; ++i) {
             int j = parameters.height()-1;
+            
             ElevationProfile profile = new ElevationProfile(dem, parameters.observerPosition(),
                     parameters.azimuthForX(i), parameters.maxDistance());
             
             double x = 0;
-            while (j >= 0 && x < Double.POSITIVE_INFINITY){
+            
+            while (j >= 0 && x < Double.POSITIVE_INFINITY) {
                 
                double lowerBoundRoot = Math2.firstIntervalContainingRoot(
-                        rayToGroundDistance(profile, parameters.observerElevation(), Math.tan(parameters.altitudeForY(j))),
-                        x, parameters.maxDistance(), step);
+                        rayToGroundDistance(profile, parameters.observerElevation(), 
+                                Math.tan(parameters.altitudeForY(j))),
+                                x, parameters.maxDistance(), step);
                
-                if (lowerBoundRoot < Double.POSITIVE_INFINITY){
-                    
-                    x = Math2.improveRoot(
-                            rayToGroundDistance(profile, parameters.observerElevation(),
+                if (lowerBoundRoot < Double.POSITIVE_INFINITY) {
+                    x = Math2.improveRoot(rayToGroundDistance(profile, 
+                                    parameters.observerElevation(),
                                     Math.tan(parameters.altitudeForY(j))),
-                            lowerBoundRoot, lowerBoundRoot + step, delta);
-                }else{
-                    
+                                    lowerBoundRoot, lowerBoundRoot + step, delta);
+                } else {
                     x = lowerBoundRoot;
                 }
-                if (x < Double.POSITIVE_INFINITY){
+                
+                if (x < Double.POSITIVE_INFINITY) {
                     float distance = (float) (x/(Math.cos(Math.abs(parameters.altitudeForY(j)))));
                     float latitude = (float) profile.positionAt(x).latitude();
                     float longitude = (float) profile.positionAt(x).longitude();
@@ -61,18 +69,13 @@ public final class PanoramaComputer {
                 --j;
             }
         }
-        
+
         return builder.build();
-        
     }
     
-    public static DoubleUnaryOperator rayToGroundDistance(ElevationProfile profile, double ray0, double raySlope){
+    public static DoubleUnaryOperator rayToGroundDistance(ElevationProfile profile, double ray0, double raySlope) {
         return x -> {
             return ray0 + x*raySlope - profile.elevationAt(x) + ((1-0.13)/(2*Distance.EARTH_RADIUS))*Math2.sq(x);
         };
-       
     }
-    
-    
-
 }
