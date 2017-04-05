a292caefae78831f480e12ee9f792374f543df10
Unknown <samuel.chassot@bluewin.ch>
2017-04-05 08:10:11 +0200

    ChannelPainter 3/4

--- src/ch/epfl/alpano/gui/ChannelPainter.java
+++ src/ch/epfl/alpano/gui/ChannelPainter.java
@@ -6,9 +6,36 @@
 
 package ch.epfl.alpano.gui;
 
+import ch.epfl.alpano.Panorama;
+
+@FunctionalInterface
 public interface ChannelPainter {
 
     public abstract float valueAt(int x, int y);
     
-    public static
+    public static ChannelPainter maxDistanceToNeighbors(Panorama pano){
+        
+        return (x,y)->
+            Math.max(Math.max(pano.distanceAt(x, y-1), pano.distanceAt(x, y+1)),
+                    Math.max(pano.distanceAt(x+1, y), pano.distanceAt(x-1, y))) - pano.distanceAt(x, y);
+        
+    }
+    
+    public default ChannelPainter add(float constant){
+        return (x,y)-> valueAt(x,y)+ constant;  
+    }
+    
+    public default ChannelPainter mul(float constant){
+        return (x,y)-> valueAt(x,y) * constant;  
+    }
+    
+    public default ChannelPainter sub(float constant){
+        return (x,y)-> valueAt(x,y) - constant;  
+    }
+   
+    public default ChannelPainter div(float constant){
+        return (x,y)-> valueAt(x,y)  / constant;  
+    }
+    
+    
 }
