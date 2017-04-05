d7e2f6aa52a2cbaeab365d96cc827a1f147e5ab0
Unknown <samuel.chassot@bluewin.ch>
2017-04-05 22:01:55 +0200

    not finished

--- src/ch/epfl/alpano/gui/PanoramaRenderer.java
+++ src/ch/epfl/alpano/gui/PanoramaRenderer.java
@@ -6,6 +6,16 @@
 
 package ch.epfl.alpano.gui;
 
+import ch.epfl.alpano.Panorama;
+import javafx.scene.image.*;
+
 public interface PanoramaRenderer {
 
+    @SuppressWarnings("restriction")
+    public static Image renderPanorama(ImagePainter painter, Panorama pano){
+        WritableImage img = new WritableImage(pano.parameters().width(), pano.parameters().height());
+        PixelWriter writer = img.getPixelWriter();
+        
+        return null;
+    }
 }
