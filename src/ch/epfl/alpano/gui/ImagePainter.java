261881b96e4361d134b80472d3aa02e1337e9c4e
Unknown <samuel.chassot@bluewin.ch>
2017-04-06 19:09:33 +0200

    doc imagepainter

--- src/ch/epfl/alpano/gui/ImagePainter.java
+++ src/ch/epfl/alpano/gui/ImagePainter.java
@@ -12,6 +12,12 @@ import javafx.scene.paint.Color;
 @SuppressWarnings("restriction")
 public interface ImagePainter {
     
+    /**
+     * gives the color of the pixel at position x,y
+     * @param x the coordinate x of the pixel
+     * @param y the coordinate y of the pixel
+     * @return a Color
+     */
     public abstract Color colorAt(int x, int y);
     
     public static ImagePainter hsb(ChannelPainter hue, 
