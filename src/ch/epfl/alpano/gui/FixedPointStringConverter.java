dd0cbbc21e17ed24a64f95f8d11aa0338b1cebaf
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-04-25 23:25:22 +0200

    FixedPointStringConverter methods added

--- src/ch/epfl/alpano/gui/FixedPointStringConverter.java
+++ src/ch/epfl/alpano/gui/FixedPointStringConverter.java
@@ -11,4 +11,16 @@ import javafx.util.StringConverter;
 
 public final class FixedPointStringConverter extends StringConverter<Integer> {
 
+    @Override
+    public Integer fromString(String string) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String toString(Integer object) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
