18c3222aed6bd7d988c355cd46ade185f579d340
Unknown <samuel.chassot@bluewin.ch>
2017-04-25 22:05:23 +0200

    add test to test Labelizer

---
+++ test/ch/epfl/alpano/gui/LabelizerTest.java
@@ -0,0 +1,20 @@
+/*
+ *	Author:      Samuel Chassot (270955)
+ *	Date:        25 avr. 2017
+ */
+
+
+package ch.epfl.alpano.gui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LabelizerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
