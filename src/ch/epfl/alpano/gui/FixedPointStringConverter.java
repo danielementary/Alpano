0b4a2ee04fbf400de68b5a74ed7656203fea1586
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-04-25 22:47:55 +0200

    FixedPointStringConverter file created

---
+++ src/ch/epfl/alpano/gui/FixedPointStringConverter.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Samuel Chassot (270955)
+ * @author Daniel Filipe Nunes Silva (275197)
+ *
+ */
+
+package ch.epfl.alpano.gui;
+
+import javafx.util.StringConverter;
+
+public final class FixedPointStringConverter extends StringConverter<Integer> {
+
+}
