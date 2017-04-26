2b0a2ec794b0e41577779962b7334c9e22d24d0d
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-04-26 17:32:06 +0200

    FixedPointStringConverter

--- src/ch/epfl/alpano/gui/FixedPointStringConverter.java
+++ src/ch/epfl/alpano/gui/FixedPointStringConverter.java
@@ -7,20 +7,28 @@
 
 package ch.epfl.alpano.gui;
 
+import java.math.BigDecimal;
+
 import javafx.util.StringConverter;
 
 public final class FixedPointStringConverter extends StringConverter<Integer> {
-
+    private final int dec;
+    
+    public FixedPointStringConverter(int dec) {
+        this.dec = dec;
+    }
+    
     @Override
     public Integer fromString(String string) {
-        // TODO Auto-generated method stub
-        return null;
+        return new BigDecimal(string).movePointRight(dec)
+                                     .setScale(0, BigDecimal.ROUND_HALF_UP)
+                                     .intValue();
     }
 
     @Override
-    public String toString(Integer object) {
-        // TODO Auto-generated method stub
-        return null;
+    public String toString(Integer i) {
+        return new BigDecimal(i).movePointLeft(dec)
+                                .toPlainString();
     }
 
 }
