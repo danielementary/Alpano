1d46c7670db3c38c093b540caa258bceae65b715
Unknown <samuel.chassot@bluewin.ch>
2017-04-27 09:09:57 +0200

    Labelizer works

--- testSD/ch/epfl/alpano/gui/JavaFXMainSD.java
+++ testSD/ch/epfl/alpano/gui/JavaFXMainSD.java
@@ -1,16 +1,13 @@
 /*
  *	Author:      Samuel Chassot (270955)
- *	Date:        25 avr. 2017
+ *	Date:        27 avr. 2017
  */
 
 
 package ch.epfl.alpano.gui;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
-
-import org.junit.Test;
 
 import ch.epfl.alpano.PanoramaParameters;
 import ch.epfl.alpano.dem.ContinuousElevationModel;
@@ -18,17 +15,18 @@ import ch.epfl.alpano.dem.DiscreteElevationModel;
 import ch.epfl.alpano.dem.HgtDiscreteElevationModel;
 import ch.epfl.alpano.summit.GazetteerParser;
 import ch.epfl.alpano.summit.Summit;
-
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Node;
-import javafx.scene.text.Text;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Translate;
-import javafx.scene.shape.Line;
+import javafx.stage.Stage;
 
-public class LabelizerTest {
+public final class JavaFXMainSD extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-    @Test
-    public void test() throws Exception {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         File HGT_FILE = new File("N46E007.hgt");
         try (DiscreteElevationModel dDEM =
                 new HgtDiscreteElevationModel(HGT_FILE)) {
@@ -41,10 +39,16 @@ public class LabelizerTest {
             Labelizer l = new Labelizer(cDEM,list);
             List<Node> n = l.labels(p);
             
-//            for (Node node : n) {
-//                System.out.println(n);
-//            }
+//            System.out.println(n.size());
+            for(int i = 0 ; i< 44 ; ++i)
+                System.out.println(n.get(i));
         }               
 
+        Platform.exit();
     }
+
+
+
+
+
 }
