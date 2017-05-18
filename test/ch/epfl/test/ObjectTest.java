f7d6e3eed566bae2bb1e5f76e669cc80238cd39a
Unknown <samuel.chassot@bluewin.ch>
2017-05-18 13:34:24 +0200

    suppresed test packages

--- test/ch/epfl/test/ObjectTest.java
+++
@@ -1,7 +0,0 @@
-package ch.epfl.test;
-
-public interface ObjectTest {
-    public static boolean hashCodeIsCompatibleWithEquals(Object o1, Object o2) {
-        return ! o1.equals(o2) || o1.hashCode() == o2.hashCode();
-    }
-}
