b7195d9fd1016a9ac8c6f9a88f6638ee8ca67003
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2017-05-31 14:50:44 +0200

    jordil 60 degrés

--- src/ch/epfl/alpano/gui/PredefinedPanoramas.java
+++ src/ch/epfl/alpano/gui/PredefinedPanoramas.java
@@ -80,7 +80,7 @@ public interface PredefinedPanoramas {
                                                     MAIN_HEIGHT, MAIN_SUPER_SAMP);
 
     public static final PanoramaUserParameters LE_JORDIL 
-                        = new PanoramaUserParameters(68844, 465664, 870, 180, 120,
+                        = new PanoramaUserParameters(68844, 465664, 870, 180, 60,
                                                      MAIN_MAX_DIST, MAIN_WIDTH,
                                                      MAIN_HEIGHT, MAIN_SUPER_SAMP);
 
